c96bf5f22cd9fb54c350a2e9fff46ada3d41aa84
Mahmood Nalim <90594423+nalim-mdosl@users.noreply.github.com>
2023-10-29 13:46:28 +0530

    Updates

---
+++ precache-manifest.51cdd98e6ec4c9dbf66815fb1228fd85.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9da4987ba8ec15ee23c2f853b36a046",
+    "url": "/live-pointofsale-frontend/index.html"
+  },
+  {
+    "revision": "60c9aabba8cfc9f19698",
+    "url": "/live-pointofsale-frontend/static/css/3.d34346ea.chunk.css"
+  },
+  {
+    "revision": "b8eb99213bf35a01d451",
+    "url": "/live-pointofsale-frontend/static/css/main.67dcb44e.chunk.css"
+  },
+  {
+    "revision": "236ea8e9a32ee807ca2a",
+    "url": "/live-pointofsale-frontend/static/js/0.1a605c13.chunk.js"
+  },
+  {
+    "revision": "2f9c54900ff362c424bd",
+    "url": "/live-pointofsale-frontend/static/js/10.8d513f1c.chunk.js"
+  },
+  {
+    "revision": "e60bb044f9e89991bc32",
+    "url": "/live-pointofsale-frontend/static/js/11.a4137c02.chunk.js"
+  },
+  {
+    "revision": "671186178017a849c500",
+    "url": "/live-pointofsale-frontend/static/js/12.f00de6c8.chunk.js"
+  },
+  {
+    "revision": "7e93c6c2624989fb933c",
+    "url": "/live-pointofsale-frontend/static/js/13.c933cdef.chunk.js"
+  },
+  {
+    "revision": "b7bf3d2420d3d3e35777",
+    "url": "/live-pointofsale-frontend/static/js/14.41581977.chunk.js"
+  },
+  {
+    "revision": "60c9aabba8cfc9f19698",
+    "url": "/live-pointofsale-frontend/static/js/3.35b033d7.chunk.js"
+  },
+  {
+    "revision": "6c972b6551acd949477d",
+    "url": "/live-pointofsale-frontend/static/js/4.790054d3.chunk.js"
+  },
+  {
+    "revision": "4068eff47bbfdef965f9",
+    "url": "/live-pointofsale-frontend/static/js/5.53bddddb.chunk.js"
+  },
+  {
+    "revision": "d04369654e887a4220a6",
+    "url": "/live-pointofsale-frontend/static/js/6.d4747732.chunk.js"
+  },
+  {
+    "revision": "5cc6064eec7699a4a9c3",
+    "url": "/live-pointofsale-frontend/static/js/7.809137c8.chunk.js"
+  },
+  {
+    "revision": "96e116c4b49ad4c1faf7",
+    "url": "/live-pointofsale-frontend/static/js/8.9375d9a8.chunk.js"
+  },
+  {
+    "revision": "9d93f1a2d1919e632d7c",
+    "url": "/live-pointofsale-frontend/static/js/9.a3190e29.chunk.js"
+  },
+  {
+    "revision": "b8eb99213bf35a01d451",
+    "url": "/live-pointofsale-frontend/static/js/main.dffe9ec2.chunk.js"
+  },
+  {
+    "revision": "7a7833b5086ed90d30ea",
+    "url": "/live-pointofsale-frontend/static/js/runtime-main.3514cd5f.js"
+  },
+  {
+    "revision": "0ab835e567a7c120c7e51527292dbc02",
+    "url": "/live-pointofsale-frontend/static/media/GAC-logo.0ab835e5.jpeg"
+  },
+  {
+    "revision": "4211cd23358f9e7f45a3bf0852d2fd66",
+    "url": "/live-pointofsale-frontend/static/media/fontello.4211cd23.woff2"
+  },
+  {
+    "revision": "5dd65aa9bff336087fc08330a06bd74e",
+    "url": "/live-pointofsale-frontend/static/media/fontello.5dd65aa9.ttf"
+  },
+  {
+    "revision": "5fc42d34842a3ea020fca942b8e62d20",
+    "url": "/live-pointofsale-frontend/static/media/fontello.5fc42d34.eot"
+  },
+  {
+    "revision": "ad65a9795380e41f878b647b478324f1",
+    "url": "/live-pointofsale-frontend/static/media/fontello.ad65a979.svg"
+  },
+  {
+    "revision": "e74c13fedc29a802bb7fb06feb401ca8",
+    "url": "/live-pointofsale-frontend/static/media/fontello.e74c13fe.woff"
+  }
+]);
