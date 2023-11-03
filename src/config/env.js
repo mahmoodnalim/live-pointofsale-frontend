43a1c489cc489b85cf91beffe0402c183c10d6ea
Mahmood Nalim <90594423+nalim-mdosl@users.noreply.github.com>
2023-11-03 22:10:20 +0530

    deployed to github pages

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,10 @@
-const devApiUrl = 'http://45.80.153.118:3300/api/';
-const prodApiUrl = 'http://45.80.153.118:3300/api/';
+// const devApiUrl = 'http://45.80.153.118:3300/api/';
+// const prodApiUrl = 'http://45.80.153.118:3300/api/';
+const prodApiUrl =
+  'https://live-pointofsale-backend-production.up.railway.app/api';
+const devApiUrl =
+  'https://live-pointofsale-backend-production.up.railway.app/api/';
+
 // const devApiUrl = 'http://localhost:3300/api/';
 
 export default {
